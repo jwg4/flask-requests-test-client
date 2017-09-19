b3be00dacd3ecd3484d658b5d3e202aa12a4bbec
Jack Grahl <jack.grahl@gmail.com>
2017-09-19 21:12:07 +0000

    Create a Response class to wrap the flask client one.

--- frtc/__init__.py
+++ frtc/__init__.py
@@ -3,4 +3,17 @@ class TestClient(object):
         self.client = client
         
     def get(self, url):
-        return self.client.get(url)
+        return Response(self.client.get(url))
+
+
+class Response(object):
+    def __init__(self, response):
+        self.response = response
+
+    @property
+    def status_code(self):
+        return self.response.status_code
+
+    @property
+    def apparent_encoding(self):
+        return 'utf-8'
