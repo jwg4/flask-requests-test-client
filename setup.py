a3b0af8f25f5cb2eb2fe12da1e76f99660fa15fd
Jack Grahl <jack.grahl@gmail.com>
2017-09-23 23:49:04 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask-test-requests-client',
-    version='0.1.0',
+    version='0.2.0',
     description='A Flask test client which replicates the requests interface.',
     url='http://github.com/jwg4/flask-test-requests-client',
     author='Jack Grahl',
